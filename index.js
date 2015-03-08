9f981b3349cd3a257ff92f7a8f14f37781048f87
ragingwind <ragingwind@gmail.com>
2015-03-09 00:00:49 +0900

    Init first version

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 var through = require('through2');
 var gutil = require('gulp-util');
+var File = gutil.File;
 var path = require('path');
+var fs = require('fs');
 var Manifest = require('chrome-manifest');
 
-module.exports = function (opts) {
-	opts = opts || {};
+module.exports = function (options) {
+	var opts = options || {};
 
   return through.obj(function(file, enc, cb) {
   	if (file.isNull()) {
@@ -19,9 +21,18 @@ module.exports = function (opts) {
   		return;
   	}
 
-  	// var manifestFile = file.clone();
-  	var manifest = new Manifest();
-  	manifest.load(JSON.parse(file.contents));
+  	var manifest = new Manifest().load(JSON.parse(file.contents));
+  	var appBasePath = path.dirname(file.path);
+		var cwd = '';
+
+		function enter() {
+			cwd = process.cwd();
+			process.chdir(appBasePath);
+		}
+
+		function pop() {
+			process.chdir(cwd);
+		}
 
   	// patch version number
   	if (typeof opts.buildnumber === 'boolean') {
@@ -35,21 +46,72 @@ module.exports = function (opts) {
   		manifest.exclude(opts.exclude);
   	}
 
-		// push scripts/styles register in manifest
-		// this.push
+  	enter();
 
-		// overwrite
-  	if (opts.overwrite) {
-  		Object.keys(opts.overwrite).forEach(function(key) {
-  			manifest.set(key, opts.overwrite[key])
-  		})
+		// make concatenated stream
+		var prop = manifest.prop('app') ? 'app.background' : 'background.scripts';
+		var backgrounds = manifest.prop(prop);
+
+  	if (opts.background) {
+  		if (!backgrounds) {
+  			throw new Error('Manifest has no background property');
+  		}
+
+			if (!Array.isArray(backgrounds)) {
+				backgrounds = new Array(backgrounds);
+			}
+
+			var contents = [];
+
+			backgrounds.map(function(src) {
+				if (opts.background.exclude && opts.background.exclude.indexOf(src) === -1) {
+					contents.push(fs.readFileSync(src));
+				}
+			});
+
+			this.push(new File({
+				path: path.resolve(opts.background.target),
+				contents: Buffer.concat(contents)
+			}));
+
+			manifest.set(prop, opts.background.target);
+  	} else if (backgrounds) {
+  		backgrounds.forEach(function(src) {
+  			this.push(new File({
+					path: path.resolve(src),
+					contents: fs.readFileSync(path.resolve(src))
+				}));
+			}.bind(this));
   	}
 
-  	this.push(new gutil.File({
+  	// streaming content resources
+  	var contentFiles = [];
+		manifest.val('content_scripts').forEach(function(r) {
+			if (r.js && r.js.length > 0) {
+				contentFiles = contentFiles.concat(r.js);
+			}
+
+			if (r.css && r.css.length > 0) {
+				contentFiles = contentFiles.concat(r.css);
+			}
+		});
+
+		// stream out files
+		contentFiles.forEach(function(src) {
+			this.push(new gutil.File({
+				path: path.resolve(src),
+				contents: fs.readFileSync(path.resolve(src))
+			}));
+		}.bind(this));
+
+		// add manifest.json
+		this.push(new File({
 			path: file.path,
 			contents: manifest.toBuffer()
 		}));
 
+		pop();
+
   	cb();
-  })
+  });
 };
